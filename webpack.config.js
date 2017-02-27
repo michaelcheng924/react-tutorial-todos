3df05984f7c72295e3bca23bf9f616e91b8c64b6
Eduardo Nogueira <1110512@isep.ipp.pt>
2017-02-27 23:26:47 +0000

    Update tutorial solution to webpack v2.

--- webpack.config.js
+++ webpack.config.js
@@ -13,20 +13,20 @@ module.exports = {
         filename: 'bundle.js'
     },
     resolve: {
-        modulesDirectories: ['node_modules', 'src'],
-        extensions: ['', '.js']
+        modules: ['node_modules', 'src'],
+        extensions: ['.js']
     },
     module: {
         loaders: [
         {
             test: /\.jsx?$/,
             exclude: /node_modules/,
-            loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015']
+            loaders: ['react-hot-loader', 'babel-loader?presets[]=react,presets[]=es2015']
         }
         ]
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
+        new webpack.NoEmitOnErrorsPlugin()
     ]
 };
